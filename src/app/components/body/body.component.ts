7435b745a37e346931aa1a364a774f77c1315698
R-V-F <100313927+R-V-F@users.noreply.github.com>
2022-06-17 17:50:54 -0300

    Guide Rect now allign with canvas pxs in all sizes

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -221,7 +221,7 @@ export class BodyComponent implements OnInit, AfterViewInit {
       // ->
       divg!.style.left = +this.scale + this.leftg + "px";
     }
-    else if(y - this.cy > this.scale/2) {
+    if(y - this.cy > this.scale/2) {
       // V
       divg!.style.top = -this.scale + this.topg + "px";
     }
@@ -288,24 +288,25 @@ export class BodyComponent implements OnInit, AfterViewInit {
     let view_top = Math.round(document.documentElement.clientHeight * 0.5);
     let view_left = Math.round(document.documentElement.clientWidth * 0.5);
 
-    //Coordinates for the midle of the screen relative to the viewport
+    //Coordinates for the midle of the screen relative to the canvas
     let mid_top = div!.offsetTop * -1 + view_top;
     let mid_left = div!.offsetLeft * -1 + view_left;
 
     //Set mid_top and mid_left to the nearest interesection
     if((mid_top % this.scale) >= this.scale/2) { //half bottom of the 40x40 square
-      mid_top += mid_top % this.scale;
+      mid_top += this.scale - (mid_top % this.scale);
     }
     else if((mid_top % this.scale) < this.scale/2) {
-      mid_top -= mid_top % this.scale;
+      mid_top -=(mid_top % this.scale);
     }
 
     if((mid_left % this.scale) >= this.scale/2) { //half right of the 40x40 square
-      mid_left += mid_left % this.scale;
+      mid_left += this.scale - (mid_left % this.scale);
     }
     else if((mid_left % this.scale) < this.scale/2) {
-      mid_left -= mid_left % this.scale;
+      mid_left -=(mid_left % this.scale);
     }
+    //(mid_left,mid_top) -> coordinates for the nearest intersection from the midle of the screen
 
     //Adjust mid_top and mid_left depending on the brush_size
     if((this.brush_size_parent % 2) == 0) { //If it's an even size brush, offset by half the size of the brush
@@ -322,28 +323,11 @@ export class BodyComponent implements OnInit, AfterViewInit {
     divg!.style.height = `${size - 1}px`;
 
     //Guide pos
-    let guide_top = 0;
-    let guide_left = 0;
-    if(((mid_top - div!.offsetTop*-1) % this.scale) <= this.scale) {
-      guide_top = (mid_top - div!.offsetTop*-1) - ((mid_top - div!.offsetTop*-1) % this.scale);
-    }
-    else {
-      guide_top = (mid_top - div!.offsetTop*-1) + ((mid_top - div!.offsetTop*-1) % this.scale);
-    }
-
-    if(((mid_left - div!.offsetLeft*-1) % this.scale) <= this.scale) {
-      console.log(`guide_left = ${(mid_left - div!.offsetLeft*-1)} - ${((mid_left - div!.offsetLeft*-1) % this.scale)}`);
-      guide_left = (mid_left - div!.offsetLeft*-1) - ((mid_left - div!.offsetLeft*-1) % this.scale);
-    }
-    else {
-      console.log(`guide_left = ${(mid_left - div!.offsetLeft*-1)} + ${((mid_left - div!.offsetLeft*-1) % this.scale)}`);
-      guide_left = (mid_left - div!.offsetLeft*-1) + ((mid_left - div!.offsetLeft*-1) % this.scale);
-    }
+    let guide_top = (mid_top - div!.offsetTop*-1);
+    let guide_left = (mid_left - div!.offsetLeft*-1);
     
     divg!.style.top = `${guide_top}px`;
     divg!.style.left = `${guide_left}px`;
-    console.log(`guide_top = ${guide_top}, guide_left = ${guide_left}`);
-;
 
     //Initial values of bx and cy updated 
     this.cx = divg!.offsetLeft;
@@ -354,7 +338,6 @@ export class BodyComponent implements OnInit, AfterViewInit {
   getBrushSize(size:any) {
     this.brush_size_parent = size;
     this.adjustGuideRectSize(size*this.scale);
-    console.log(`b size:${this.brush_size_parent}`);
   }
 
 
@@ -364,11 +347,10 @@ export class BodyComponent implements OnInit, AfterViewInit {
  * 
  * 
  * TODO:
- * # Align Painting => Odd numbers are always off by 20px (half scale)
- * #                => Even numbers off by a few pixels, might aply to odds too
- * # Speed up guideRect when dragging -> It's taking a lot of time for the guide to catch up
  * # cx, cy => maybe change to initial_x, initial_y or natural_x, natural_y
  * # topg and leftg => guide_y_delta, guide_x_delta
- * w
+ * 
+ * # Align painting with guide positioning
+ * # Add color pallet
  */
 
