371f03c6b151f9d44fcd6e85036ca9e46dc5d890
R-V-F <100313927+R-V-F@users.noreply.github.com>
2022-07-06 08:53:16 -0300

    Add: Hosting setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAXaFZfApG_7fgC1R4eEe2qv1MdFEJOLvw",
+    authDomain: "dotcanvas-b2cb6.firebaseapp.com",
+    projectId: "dotcanvas-b2cb6",
+    storageBucket: "dotcanvas-b2cb6.appspot.com",
+    messagingSenderId: "17330917183",
+    appId: "1:17330917183:web:3f19c6cb457970ef8f4d8a"
+  }
 };
